0fa516991e65e7b5b125c319cec0a6341f62262c
= <perpetualcoder@gmail.com>
2023-07-30 14:41:35 +0100

    Refactor dashboard

--- bill_pay_proj/settings.py
+++ bill_pay_proj/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG'))
 
 if not DEBUG:
     ALLOWED_HOSTS = ['143.198.158.39']
